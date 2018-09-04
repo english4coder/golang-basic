16b5126ef7462a3f37a6965f490f5302482ae46d
Paco <williambai100@163.com>
2018-09-04 21:36:41 +0800

    word reversion can be implemented easily

---
+++ reverse.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "the weather"
+	r := []rune(str)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	fmt.Println(string(r))
+}

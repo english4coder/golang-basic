1d5cbb962263d44c04ea60a3c3368f606275355c
Paco <williambai100@163.com>
2018-09-07 20:02:41 +0800

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 6, 8
+	fmt.Println(b, c)
+
+	var d bool = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+}
